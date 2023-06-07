98aa8914bbc8253ae266fbe7e5f4c80c25998350
Zachary Sousa <zs20np@snpsteam.com>
2023-06-07 15:06:33 -0400

    Added Car and some infrastructure for the World

---
+++ AncestorGame.java
@@ -0,0 +1,48 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
+
+/**
+ * Abstract world for storing global variables and functions that actors may need. The ACTUAL
+ * 'GAME' world will inherit from this one. When creating a reference to the world for your
+ * actor class, you may find it useful to use the AbstGame Object type.
+ * 
+ * @author      Zachary Sousa 
+ * @version     1.00
+ */
+public class AncestorGame extends World
+{
+    /*vv GLOBAL VARIABLES vv*/
+    protected double globalSpeed;
+    /*^^ GLOBAL VARIABLES ^^*/
+    
+    /**
+     * Calls the constructor of the super class world to generate the canvas.
+     * 
+     * @author  Zachary Sousa
+     * @version 1.00
+     */
+    public AncestorGame(int width, int height) {    
+        super(width, height, 1);
+    }
+    
+    /*vv USEFUL FUNCTIONS vv*/
+    /**
+     * getGlobalSpeed
+     * @return globalSpeed
+     * 
+     * @author  Zachary Sousa
+     * @version 1.00
+     */
+    public double getGlobalSpeed() {
+        return globalSpeed;
+    }
+    
+    /**
+     * removes the given Object from the world and removes it from the list it originates from.
+     */
+    public void despawn(Actor obj, ArrayList<Actor> list) {
+        removeObject(obj);
+        list.remove(obj);
+    }
+    
+}
---
+++ RoadObject.java
@@ -0,0 +1,70 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
+
+/**
+ * RoadObject moves with the screen using the global speed of travel.
+ * 
+ * @author      Zachary Sousa
+ * @version     1.00
+ */
+public class RoadObject extends SmoothMover implements Despawnable
+{
+    private Vector pos;                 //the position of the Car on the screen
+    private double globalSpeed;         //the rate that the world is moving at
+    private double speedMultiplier = 1; //multiplies the speed, 1 for down, -1 for up
+    protected AncestorGame game;
+    protected ArrayList<Actor> list;
+    
+    /**
+     * Constructor for the RoadObject, initializes the Vector position.
+     * 
+     * @author  Zachary Sousa
+     * @version 1.00
+     */
+    public RoadObject(AncestorGame game, ArrayList<Actor> list) {
+        pos = new Vector(getX(), getY());
+        this.game = game;
+        this.list = list;
+    }
+    
+    /**
+     * act is called once per frame. The RoadObject moves downward (or upward if 
+     * speedMultiplier is set to a negative number).
+     * 
+     * @author  Zachary Sousa
+     * @version 1.00
+     */
+    public void act() {
+        globalSpeed = game.getGlobalSpeed();
+        pos.add(0, globalSpeed * speedMultiplier);
+        
+        setLocation(pos.getX(), pos.getY());    //update actor position to match pos
+    }
+    
+    /**
+     * returns the Vector position of the RoadObject.
+     * 
+     * @author  Zachary Sousa
+     * @version 1.00
+     */
+    public Vector getPos() {
+        return pos;
+    }
+    
+    /**
+     * sets the globalSpeed of the RoadObject
+     * 
+     * @author  Zachary Sousa
+     * @version 1.00
+     */
+    public void setGlobalSpeed(double globalSpeed) {
+        this.globalSpeed = globalSpeed;
+    }
+    
+    /**
+     * 
+     */
+    public void despawn(Actor a, ArrayList<Actor> list) {
+        
+    }
+}
